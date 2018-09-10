be6d02177fa7bd6d90c40ebcc04f8b354ee8c70e
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-09-10 12:22:39 +0300

    Add files via upload

---
+++ lab3_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+temperat_Far = int(input('Введіть значення за шкалою Фарангейта:'))
+temperat_Cel = (temperat_Far - 32) * (5 / 9)
+print('Градуси за шкалою Цельсія: ', temperat_Cel)
+
