90ce1d9c54a0c11eeaee8e085409caa2baa157e7
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-09-10 12:37:01 +0300

    Add files via upload

---
+++ lab3_3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+zrist = int(input('Введіть зріст: '))
+vaga = int(input ('Введіть вагу: '))
+BodyMassIndex = vaga // (zrist ** 2)
+print('Індекс маси тіла: ',BodyMassIndex)
