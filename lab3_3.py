5afd7be48d15522c026fe4576121bee78ace9079
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-09-23 13:42:18 +0300

    Update lab3_3.py

--- lab3_3.py
+++ lab3_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-zrist = float(input('Введіть зріст: '))
-vaga = float (input ('Введіть вагу: '))
+zrist = float(input('Введіть зріст, в м : '))
+vaga = float (input ('Введіть вагу, в кг: '))
 BodyMassIndex = float( vaga / (zrist ** 2))
 print('Індекс маси тіла: ',BodyMassIndex)
