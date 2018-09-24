4912a6d55eabd0485e69279ea8dbaaf97afdc0fc
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-09-24 12:13:16 +0300

    Update lab5_2.py

--- lab5_2.py
+++ lab5_2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-h = int(input('Введіть висоту дверей: '))
-w = int(input('Введіть ширину дверей: '))
+h = float(input('Введіть висоту дверей: '))
+w = float(input('Введіть ширину дверей: '))
 
-a = int(input('Введіть висоту коробки: '))
-b = int(input('Введіть ширину коробки: '))
-c = int(input('Введіть довжину коробки: '))
+a = float(input('Введіть висоту коробки: '))
+b = float(input('Введіть ширину коробки: '))
+c = float(input('Введіть довжину коробки: '))
 
-if (a <= h or a<= w) and (b <= h or b<= w):
+if (a <= h or a <= w) and (b <= h or b <= w):
  print('Ви можете помістити коробку через двері.')
-elif  (a <= h or a<= w) and (c <= h or c<= w):
+elif  (a <= h or a <= w) and (c <= h or c <= w):
  print('Ви можете помістити коробку через двері.')
-elif  (b <= h or b<= w) and (c <= h or c<= w):
+elif  (b <= h or b <= w) and (c <= h or c <= w):
  print('Ви можете помістити коробку через двері.')
 else:
  print('Ви не можете помістити коробку через двері.')
