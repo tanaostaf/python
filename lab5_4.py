905c74ce21aaf68cbca205a5c0129df81dddb272
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-09-24 11:28:46 +0300

    Update lab5_4.py

--- lab5_4.py
+++ lab5_4.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import math
 print ('Коефіцієнти рівняння')
 
-a = float ( input ("a : "))
-b = float ( input ("b : "))
-c = float ( input ("c : "))
-import math
+a = float(input ("a : "))
+b = float(input ("b : "))
+c = float(input ("c : "))
 
 D = (b**2)-4*a*c
 print ('Дискримінант: ',D)
@@ -15,9 +14,25 @@ if D < 0:
 elif D == 0:
      x = -b/(2*a)
      print ('Корені рівняння: ',x1)
+elif b == 0:
+     x1 = math.sqrt(-c/a)
+     x2 = -(math.sqrt(-c/a))
+     print(x1)
+     print(x2)
+elif c == 0:
+     x1 = 0
+     x2 = -b/a
+     print(x1)
+     print(x2)
+elif b == 0 or c == 0:
+     x1 = 0
+     x2 = 0
+     print(x1)
+     print(x2)
 else:
      x1 = (-b+math.sqrt(D))/(2*a)
      x2 = (-b-math.sqrt(D))/(2*a) 
      print(x1)
      print(x2)
+
 
