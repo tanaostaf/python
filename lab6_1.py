06263d0b6252898c0a55e2a794cfb12f6bbf07f6
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-10-01 11:56:48 +0300

    Update lab6_1.py

--- lab6_1.py
+++ lab6_1.py
@@ -4,8 +4,8 @@
 import math
 
 a = float(input('Vedit storony a: '))
-b = float(input('Vedit storony b:'))
-c = float(input('Vedit storony c:'))
+b = float(input('Vedit storony b: '))
+c = float(input('Vedit storony c: '))
 
 def ploshcha():
     """Ploshcha trykutnyka"""
@@ -14,8 +14,7 @@ def ploshcha():
     global c
     p = ((a + b + c) / 2)
     s = math.sqrt(p * (p - a) * (p - b) * (p - c))
-    print ('Ploshcha trykutnyka: ',s)
+    print ('Ploshcha trykutnyka: ', s)
     return s
 
 ploshcha ()
-
