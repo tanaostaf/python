e00f057ce0710d85f5e0a4d28935af6ecc09b5a0
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-10-01 11:24:26 +0300

    Add files via upload

---
+++ lab6_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+a = float(input('Vedit storony a: '))
+b = float(input('Vedit storony b:'))
+c = float(input('Vedit storony c:'))
+
+def ploshcha():
+    """Ploshcha trykutnyka"""
+    global a
+    global b
+    global c
+    p = ((a + b + c) / 2)
+    s = math.sqrt(p * (p - a) * (p - b) * (p - c))
+    print ('Ploshcha trykutnyka: ',s)
+    return s
+
+ploshcha ()
+
