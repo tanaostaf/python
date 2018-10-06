7fb221ab3d55735401404329ccb24df260371ff9
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-10-06 18:09:43 +0300

    Update lab6_1.py

--- lab6_1.py
+++ lab6_1.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
+import math 
 
-a = float(input('Vedit storony a: '))
-b = float(input('Vedit storony b: '))
-c = float(input('Vedit storony c: '))
-
-def ploshcha():
-    """Ploshcha trykutnyka"""
-    global a
-    global b
-    global c
-    p = ((a + b + c) / 2)
+def storona():
+    """Vvedennya danykh"""
+    a = float(input('Vvedit storony a: '))
+    b = float(input('Vvedit storony b: '))
+    c = float(input('Vvedit storony c: '))
+    return a, b, c
+	
+def suma_stor(parametru):
+    """Rozrakhunok ploshchi trykutnyka"""
+    a = parametru[0]
+    b = parametru[1]
+    c = parametru[2]
+    p = (a + b + c)/2 
     s = math.sqrt(p * (p - a) * (p - b) * (p - c))
-    print ('Ploshcha trykutnyka: ', s)
-    return s
+    print ('Plosha trukutnuka: ', s)
+    return s	
+parametru = storona()
+suma_stor(parametru)
+input ('Press ENTER to exit')
 
-ploshcha ()
+
