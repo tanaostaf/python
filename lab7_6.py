35bfe4c4ef2854c6a187994abfb5bcb6e9a3002c
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-10-08 12:01:49 +0300

    Add files via upload

---
+++ lab7_6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re 
+
+s = input('Vedit radok: ')
+Len = len(s)
+
+print (Len)
+print ('*'*Len,'*')
+print ('*'+s+'*')
+print ('*'*Len,'*')
+
+
+
