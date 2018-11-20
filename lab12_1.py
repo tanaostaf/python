6c76e50f65f70313f33fad915c5b087fd65736ad
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2018-11-20 13:02:42 +0200

    Update lab12_1.py

--- lab12_1.py
+++ lab12_1.py
@@ -20,47 +20,48 @@ class Point:
         dist = math.sqrt(xdiff**2 + ydiff**2) # відстань між двома точками 
         return dist #Повертаємо знайдене значення
 		
-print ('vvedit koordunatu tochku A: ' ) #ввести координати 1-ої точки
+print ('vvedit koordynaty tochky A: ' ) #ввести координати 1-ої точки
 x1 = int(input ())  #Вводимо координату по х
 y1 = int(input ())  # Вводимо координату по y
 
-print ('vvedit koordunatu tochku B: ')
+print ('vvedit koordynaty tochky B: ')
 x2 = int(input ())
 y2 = int(input ())
 
-print ('vvedit koordunatu tochku C: ')
+print ('vvedit koordynaty tochky C: ')
 x3 = int(input ())
 y3 = int(input ())
 
 A = Point(x1,y1)
 B = Point(x2,y2)
 C = Point(x3,y3)
-AB = A.distance(B)
-AC = A.distance(C)
-BC = B.distance(C)
-print('AB = ',AB)
-print ('AC = ',AC)
-print ('BC = ',BC)
 
 class trykutnyk:
-    def __init__(self,AB,AC,BC):
-        self.AB = float(AB)
-        self.AC = float(AC)
-        self.BC = float(BC)
-    def perevirka_chy_isnuye(self): #функція на перевірку чи існує трикутник 
-        if (self.AB + self.AC > self.BC) and (self.AB + self.BC > self.AC) and (self.AC + self.BC > self.AB):
-           return True
+    def __init__(self,A,B,C):
+        self.A = A
+        self.B = B
+        self.C = C
+    AB = A.distance(B)
+    BC = B.distance(C)
+    CA = C.distance(A)
+    print('AB = ',AB)
+    print ('BC = ',BC)
+    print ('CA = ',CA)
+    def triangle_check(self): #функція на перевірку чи існує трикутник
+        result = None
+        if (self.AB + self.BC > self.CA) and (self.AB + self.CA > self.BC) and (self.CA + self.BC > self.AB):
+           result = True
         else:
-           return False 
+           result = False 
+        return result
     def ploshcha(self): # функція обчислення площі трикутника 
-        perym = self.AB + self.AC + self.BC # знаходимо периметр трикутника 
-        print ('perymetr: ',perym)
+        perym = self.AB + self.BC + self.CA # знаходимо периметр трикутника 
+        print ('Perymetr: ',perym)
         piv_perym=(perym)/2 #Знаходимо півпериметр 
-        s = math.sqrt(piv_perym * (piv_perym - self.AB) * (piv_perym - self.AC) * (piv_perym - self.BC)) # обчислення площі 
+        s = math.sqrt(piv_perym * (piv_perym - self.AB) * (piv_perym - self.BC) * (piv_perym - self.CA)) # обчислення площі 
         return s #Повертаємо площу трикутника 
-
-tryk = trykutnyk(AB, AC, BC)
-print("Chy isnuye trykutnyk: ",tryk.perevirka_chy_isnuye())
-print("ploshcha : {}".format(tryk.ploshcha())) 
+tryk = trykutnyk(A, B, C)
+print("Chy isnuie trykutnyk: ",tryk.triangle_check())
+print("Ploshcha : {}".format(tryk.ploshcha())) 
 
 input ('Press ENTER to exit')
