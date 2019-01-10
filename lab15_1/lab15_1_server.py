aab04dd2dbe966705d5ed5c809f69080a34b0497
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2019-01-10 19:20:40 +0200

    Update lab15_1_server.py

--- lab15_1/lab15_1_server.py
+++ lab15_1/lab15_1_server.py
@@ -1,35 +1,39 @@
-#!/usr/bin/env python3
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import socket 
+import socket
+import time
+import datetime
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-host = '127.0.0.1'                           
-port = 9999 
-                                          
-serversocket.bind((host, port))                                  
-serversocket.listen(5)
-print('the server is waiting for connection')        
-                                    
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = 9999
+server_socket.bind((host, port))
+server_socket.listen(5)
+print('the server is waiting for connection')
 while True:
-    clientsocket, addr = serversocket.accept()      
-    print('Got a connection from {}'.format(addr))
-    
-    clientsocket.send('Hello'.encode('utf-8'))
-    client_answer = clientsocket.recv(1024)
+    client_socket, addr = server_socket.accept()
+    print("Отримано з'єднання від {}".format(addr))
+
+    client_socket.send('Як тебе звати ?'.encode('utf-8'))
+    client_answer = client_socket.recv(1024)
     print(client_answer.decode('utf-8'))
+    client_socket.send((('Привіт, ' + client_answer.decode('utf-8')) + '\n').encode('utf-8'))
+    while True:
+        client_socket.send("Що ти хочеш мене спитати?".encode('utf-8'))
+        client_answer = client_socket.recv(1024)
+        response = client_answer.decode('utf-8')
 
-    clientsocket.send('What is your name?'.encode('utf-8'))
-    client_answer = clientsocket.recv(1024)
-    print(client_answer.decode('utf-8'))
-    clientsocket.send('Hello, '.encode('utf-8') + client_answer)
-   
-    clientsocket.send('My name Ruuuu, '.encode('utf-8'))
-    client_answer = clientsocket.recv(1024)
-    print(client_answer.decode('utf-8'))
-    
-    clientsocket.close()
+        if response == 'Вихід.':
+            client_socket.close()
+            break
 
+        if response == 'Котра година ?':
+            client_socket.send(datetime.datetime.fromtimestamp(time.time()).strftime('Поточний година: %Y-%m-%d '
+                                                                                     '%H:%M:%S\n').encode('utf-8'))
+        if response == 'Скільки місяців у році?':
+            client_socket.send('Рік має 12 місяців.'.encode('utf-8'))
 
-
+        if response == 'Скільки днів у році?':
+            client_socket.send('У році є 365 днів.'.encode('utf-8'))
+    print('Клієнт відключений')
