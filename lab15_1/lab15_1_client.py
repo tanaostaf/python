75c791b6ceb286bf3bd95bac4978738a7b9e05f3
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2019-01-10 19:20:17 +0200

    Update lab15_1_client.py

--- lab15_1/lab15_1_client.py
+++ lab15_1/lab15_1_client.py
@@ -1,33 +1,22 @@
-#!/usr/bin/env python3
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import socket 
+import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-
-host = '127.0.0.1'                           
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
 port = 9999
-
-s.connect((host, port)) 
-                              
-server_responce = s.recv(1024)
-print(server_responce.decode('utf-8'))
-
+s.connect((host, port))
+server_response = s.recv(1024)
+print(server_response.decode('utf-8'))
 a = input()
-s.send(a.encode('utf-8'))    
-server_responce = s.recv(1024)                                
-print(server_responce.decode('utf-8'))
-
-b = input()
-s.send(b.encode('utf-8'))
-server_responce = s.recv(1024)                                
-print(server_responce.decode('utf-8'))
-
-c = input()
-s.send(c.encode('utf-8'))
-server_responce = s.recv(1024)                                
-print(server_responce.decode('utf-8'))
-
+s.send(a.encode('utf-8'))
+server_response = s.recv(1024)
+print(server_response.decode('utf-8'))
+while True:
+    a = input()
+    s.send(a.encode('utf-8'))
+    if a == "Вихід":
+        break
+    print(s.recv(1024).decode('utf-8'))
 s.close()
-
-input ('Press ENTER to exit') 
